3338925af49dac9c5cefaa3a5d889f1915581d4c
KonsCha <deathmorning@mail.ru>
2021-03-08 16:29:58 +0300

    создал фунцию getData

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 import {deactiveState, activeState} from './form.js';
 import {createCard} from './card.js';
 import {ADVERTISEMENT_NUMBER, createAdvertisement} from './data.js';
-import {createFetch} from './server-data.js';
+import {getData} from './server-data.js';
 
 const TOKYO_LAT = 35.6895;
 const TOKYO_LNG = 139.692;
@@ -9,63 +9,124 @@ const TOKYO_LNG = 139.692;
 const adForm = document.querySelector('.ad-form');
 const inputAddress = adForm.querySelector('#address');
 
-const createMarker = (lat, lng, icon) => {
-  return window.L.marker({
-    lat,
-    lng,
-  },
-  {
-    draggable: true,
-    icon,
-  })
-}
+// const createMarker = (lat, lng, icon) => {
+//   return window.L.marker({
+//     lat,
+//     lng,
+//   },
+//   {
+//     draggable: true,
+//     icon,
+//   })
+// }
 
-const createPin = (lat, lng) => {
-  return createMarker(lat, lng, window.L.icon({
-    iconUrl: 'img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  }));
-}
+// const createPin = (lat, lng) => {
+//   return createMarker(lat, lng, window.L.icon({
+//     iconUrl: 'img/pin.svg',
+//     iconSize: [40, 40],
+//     iconAnchor: [20, 40],
+//   }));
+// }
 
-const createMainPin = (lat, lng) => {
-  return createMarker(lat, lng, window.L.icon({
-    iconUrl: 'img/main-pin.svg',
-    iconSize: [46, 46],
-    iconAnchor: [23, 46],
-  }));
-}
+// const createMainPin = (lat, lng) => {
+//   return createMarker(lat, lng, window.L.icon({
+//     iconUrl: 'img/main-pin.svg',
+//     iconSize: [46, 46],
+//     iconAnchor: [23, 46],
+//   }));
+// }
 
-deactiveState();
+// deactiveState();
 
-const map = window.L.map('map-canvas')
-  .on('load', () => {
-    activeState();
-    inputAddress.value = `${TOKYO_LAT}, ${TOKYO_LNG}`;
-  })
-  .setView({
-    lat: TOKYO_LAT,
-    lng: TOKYO_LNG,
-  }, 8);
-window.L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
+// const map = window.L.map('map-canvas')
+//   .on('load', () => {
+//     activeState();
+//     inputAddress.value = `${TOKYO_LAT}, ${TOKYO_LNG}`;
+//   })
+//   .setView({
+//     lat: TOKYO_LAT,
+//     lng: TOKYO_LNG,
+//   }, 8);
+// window.L.tileLayer(
+//   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//   {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   },
+// ).addTo(map);
 
-const redPin = createMainPin(TOKYO_LAT, TOKYO_LNG);
+// const redPin = createMainPin(TOKYO_LAT, TOKYO_LNG);
 
-redPin.on('moveend', (evt) => {
-  inputAddress.value = `${evt.target.getLatLng().lat.toFixed(5)},
-  ${evt.target.getLatLng().lng.toFixed(5)}`;
-});
+// redPin.on('moveend', (evt) => {
+//   inputAddress.value = `${evt.target.getLatLng().lat.toFixed(5)},
+//   ${evt.target.getLatLng().lng.toFixed(5)}`;
+// });
 
-redPin.addTo(map);
+// redPin.addTo(map);
 
-const allAdvertisements = new Array(ADVERTISEMENT_NUMBER).fill().map(createAdvertisement);
-allAdvertisements.forEach((advertisement) => {
+// const allAdvertisements = new Array(ADVERTISEMENT_NUMBER).fill().map(createAdvertisement);
+// allAdvertisements.forEach((advertisement) => {
+//   const pin = createPin(advertisement.location.x, advertisement.location.y);
+//   pin.addTo(map);
+//   pin.bindPopup(() => createCard(createFetch));
+// });
+getData((data) => {
+  const createMarker = (lat, lng, icon) => {
+    return window.L.marker({
+      lat,
+      lng,
+    },
+    {
+      draggable: true,
+      icon,
+    })
+  }
+
+  const createPin = (lat, lng) => {
+    return createMarker(lat, lng, window.L.icon({
+      iconUrl: 'img/pin.svg',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    }));
+  }
+
+  const createMainPin = (lat, lng) => {
+    return createMarker(lat, lng, window.L.icon({
+      iconUrl: 'img/main-pin.svg',
+      iconSize: [46, 46],
+      iconAnchor: [23, 46],
+    }));
+  }
+
+  deactiveState();
+
+  const map = window.L.map('map-canvas')
+    .on('load', () => {
+      activeState();
+      inputAddress.value = `${TOKYO_LAT}, ${TOKYO_LNG}`;
+    })
+    .setView({
+      lat: TOKYO_LAT,
+      lng: TOKYO_LNG,
+    }, 8);
+  window.L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  const redPin = createMainPin(TOKYO_LAT, TOKYO_LNG);
+
+  redPin.on('moveend', (evt) => {
+    inputAddress.value = `${evt.target.getLatLng().lat.toFixed(5)},
+    ${evt.target.getLatLng().lng.toFixed(5)}`;
+  });
+
+  redPin.addTo(map);
+  const allAdvertisements = new Array(ADVERTISEMENT_NUMBER).fill().map(data);
+  allAdvertisements.forEach((advertisement) => {
   const pin = createPin(advertisement.location.x, advertisement.location.y);
   pin.addTo(map);
-  pin.bindPopup(() => createCard(createFetch));
+  pin.bindPopup(() => data());
+  });
 });
--- js/server-data.js
+++ js/server-data.js
@@ -1,21 +1,13 @@
-import {createCard} from './card.js';
-
-
-const createFetch = (onSuccess, onError) => () => {
-  return fetch('https://22.javascript.pages.academy/keksobooking/data')
+const getData = (onSuccess, onError) => {
+  fetch('https://22.javascript.pages.academy/keksobooking/data')
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
-
-      throw new Error(`${response.status} ${response.statusText}`);
+      showAlert('Не удалось загрузить данные с сервера :(');
     })
-    .then((json) => {
-      onSuccess(json);
-    })
-    .catch((err) => {
-      onError(err);
-    });
+    .then(onSuccess)
+    .catch(onError);
 };
 
-export {createFetch};
+export {getData};
